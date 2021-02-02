5bd11a96dbf22501cee3c5ff14d5240fba2f4cfa
Ryan <rselliott99@gmail.com>
2021-02-02 16:10:58 -0500

    Add method to determine closest point

--- fractals.js
+++ fractals.js
@@ -1,9 +1,12 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+;
 class FractalApp {
     constructor() {
         this.canvas = document.getElementById("canvas");
         this.context = this.canvas.getContext("2d");
         this.points = [];
+        this.last_position = { x: 0, y: 0 };
         this.createClickEvents();
     }
     createClickEvents() {
@@ -14,6 +17,15 @@ class FractalApp {
             click = this.getClickLocation(e);
             this.drawPoint(click, click_counter++);
         });
+        // Set our initial starting position to be between 
+        if (click_counter === 2) {
+            let starting_x = (this.points[0].x + this.points[1].x) / 2;
+            let starting_y = (this.points[0].y + this.points[1].y) / 2;
+            this.last_position = {
+                x: starting_x,
+                y: starting_y
+            };
+        }
     }
     getClickLocation(e) {
         const bounds = this.canvas.getBoundingClientRect();
@@ -25,12 +37,29 @@ class FractalApp {
         };
     }
     drawPoint(location, click_counter) {
+        // Draw a ciruclar point on the Canvas at the click location
         this.context.beginPath();
         this.context.arc(location.x, location.y, 2, 0, 2 * Math.PI);
         this.context.fill();
+        // Label the point
         this.context.font = "10px sans-serif";
         this.context.fillText(String(click_counter), location.x + 10, location.y);
+        // Add the point to our array of points
         this.points.push(location);
+    }
+    // A helper method to find the closest point to a given position
+    findClosestPoint(pos) {
+        let closest_dist = Number.MAX_VALUE;
+        let closest_point = pos;
+        // Go through all points and find whichever one is closest
+        for (const point of this.points) {
+            let dist = Math.sqrt(Math.pow((point.x - pos.x), 2) + Math.pow((point.y - pos.y), 2));
+            if (dist < closest_dist) {
+                closest_dist = dist;
+                closest_point = { x: point.x, y: point.y };
+            }
+        }
+        return closest_point;
     }
 }
 let app = new FractalApp();
--- fractals.ts
+++ fractals.ts
@@ -1,28 +1,48 @@
+import { isBindingElement, parseConfigFileTextToJson } from "typescript";
+
+interface position {
+    x: number,
+    y: number
+};
+
 class FractalApp {
     private canvas: HTMLCanvasElement;
     private context: CanvasRenderingContext2D;
-    private points: {x: number, y: number}[];
+    private points: position[];
+    private last_position: position;
 
     constructor() {
         this.canvas = document.getElementById("canvas") as HTMLCanvasElement;
         this.context = this.canvas.getContext("2d") as CanvasRenderingContext2D;
         this.points = [];
+        this.last_position = {x: 0, y: 0};
 
         this.createClickEvents();
     }
 
     private createClickEvents(): void {
         let canvas: HTMLCanvasElement = this.canvas;
-        let click: {x: number, y: number};
+        let click: position;
         let click_counter: number = 1;
 
         canvas.addEventListener("mousedown", (e: MouseEvent) => {
             click = this.getClickLocation(e);
             this.drawPoint(click, click_counter++);
         });
+
+        // Set our initial starting position to be between 
+        if (click_counter === 2) {
+            let starting_x = (this.points[0].x + this.points[1].x) / 2;
+            let starting_y = (this.points[0].y + this.points[1].y) / 2;
+
+            this.last_position = {
+                x: starting_x,
+                y: starting_y
+            };
+        }
     }
 
-    private getClickLocation(e: MouseEvent): {x: number, y: number} {
+    private getClickLocation(e: MouseEvent): position {
         const bounds = this.canvas.getBoundingClientRect();
         const click_x = e.clientX - bounds.left;
         const click_y = e.clientY - bounds.top;
@@ -33,14 +53,35 @@ class FractalApp {
         };
     }
 
-    private drawPoint(location: {x: number, y: number}, click_counter: number): void {
+    private drawPoint(location: position, click_counter: number): void {
+        // Draw a ciruclar point on the Canvas at the click location
         this.context.beginPath();
         this.context.arc(location.x, location.y, 2, 0, 2 * Math.PI);
         this.context.fill();
+
+        // Label the point
         this.context.font = "10px sans-serif";
         this.context.fillText(String(click_counter), location.x + 10, location.y);
 
-        this.points.push(location)
+        // Add the point to our array of points
+        this.points.push(location);
+    }
+    
+    // A helper method to find the closest point to a given position
+    private findClosestPoint(pos: position): position {
+        let closest_dist: number = Number.MAX_VALUE;
+        let closest_point: position = pos;
+
+        // Go through all points and find whichever one is closest
+        for (const point of this.points) {
+            let dist = Math.sqrt(Math.pow((point.x - pos.x), 2) + Math.pow((point.y - pos.y), 2));
+            if (dist < closest_dist) {
+                closest_dist = dist;
+                closest_point = { x: point.x, y: point.y };
+            }
+        }
+
+        return closest_point;
     }
 }
 
