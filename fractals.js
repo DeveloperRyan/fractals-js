3d28d1d5484e82e9ee090209ca86a7565b36a95d
Ryan <rselliott99@gmail.com>
2021-02-02 09:14:04 -0500

    Add basic click handling

--- fractals.js
+++ fractals.js
@@ -3,6 +3,23 @@ class FractalApp {
     constructor() {
         this.canvas = document.getElementById("canvas");
         this.context = this.canvas.getContext("2d");
+        this.createClickEvents();
+    }
+    createClickEvents() {
+        let canvas = this.canvas;
+        let click;
+        canvas.addEventListener("mousedown", (e) => {
+            click = this.getClickLocation(canvas, e);
+        });
+    }
+    getClickLocation(canvas, e) {
+        const bounds = canvas.getBoundingClientRect();
+        const click_x = e.clientX - bounds.left;
+        const click_y = e.clientY - bounds.top;
+        return {
+            x: click_x,
+            y: click_y
+        };
     }
     drawPoint(x, y) {
         this.context.beginPath();
--- fractals.ts
+++ fractals.ts
@@ -5,6 +5,28 @@ class FractalApp {
     constructor() {
         this.canvas = document.getElementById("canvas") as HTMLCanvasElement;
         this.context = this.canvas.getContext("2d") as CanvasRenderingContext2D;
+
+        this.createClickEvents();
+    }
+
+    private createClickEvents(): void {
+        let canvas: HTMLCanvasElement = this.canvas;
+        let click: {x: number, y: number};
+
+        canvas.addEventListener("mousedown", (e: MouseEvent) => {
+            click = this.getClickLocation(canvas, e);
+        });
+    }
+
+    private getClickLocation(canvas: HTMLCanvasElement, e: MouseEvent): {x: number, y: number} {
+        const bounds = canvas.getBoundingClientRect();
+        const click_x = e.clientX - bounds.left;
+        const click_y = e.clientY - bounds.top;
+        
+        return {
+            x: click_x,
+            y: click_y
+        };
     }
 
     drawPoint(x: number, y: number): void {
